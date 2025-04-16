7b8fa4fb8d200ffcdbcc0dc1bc990227a36a625b
Lázaro Gabriel <114248943+lazarobo@users.noreply.github.com>
2025-04-16 14:29:16 +0000

    add: fibonacci.cpp recursion test

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 
-int main(){
+int fibonacci(int n){
 
+    if (n == 0 || n == 1)
+    {
+      return n;
+    } else
+    {
+      return fibonacci(n-1) + fibonacci(n-2);
+    }
+    return 0;
+}
 
+int main() {
+  int n = 5;
+  int resultado = fibonacci(n);
+  std::cout << "Fibonacci de " << n << " é " << resultado << std::endl;
   return 0;
 }
